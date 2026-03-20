4dfa21c7621b401523f6d158865dd7ac90052a13
Prithu Sarkar <prithusarkar90@gmail.com>
2026-03-20 11:09:54 +0530

    feat: add app screenshot, README, notebook and dataset to Thunderstorm Forecasting folder

--- Thunderstorm Forecasting with MLFlow Tracking/thunderstrom-forecasting-project-full/thunderstrom-forecasting-project-full/app/model_loader.py
+++
@@ -1,9 +0,0 @@
-import joblib 
-from app.config import MODEL_PATH
-
-def load_model():
-    with open(MODEL_PATH, "rb") as f:
-        model = joblib.load(f)
-    return model
-
-model = load_model()  # Singleton loaded once
